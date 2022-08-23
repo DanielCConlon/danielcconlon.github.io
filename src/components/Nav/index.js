8134f62a3fe659e54362a951d7c561a907e709ba
Daniel Conlon <danielcconlon@gmail.com>
2022-08-23 14:22:18 -0400

    setup about and nav components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <header>
+            <h2>
+                <a href="/">
+                    Portfolio
+                </a>
+            </h2>
+        </header>
+    );
+}
+
+export default Nav;
