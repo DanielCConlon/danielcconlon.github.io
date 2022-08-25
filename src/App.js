bb67548c643ca575f3775d1c3649ea96c6faf6df
Daniel Conlon <danielcconlon@gmail.com>
2022-08-24 21:25:48 -0400

    starting over

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import React from "react";
 import About from './components/About';
-import Nav from './components/Nav';
-import Gallery from './components/Gallery';
+import Portfolio from './components/Portfolio';
 
 function App() {
+
+
+
   return (
     <div>
-      <Nav />
       <main>
-        <About />
       </main>
     </div>
   );
